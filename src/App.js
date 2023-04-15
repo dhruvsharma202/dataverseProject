997326c43c4212aa4f70c21e26e74448a9da907b
DHRUV SHARMA <dhruv.21b0131005@abes.ac.in>
2023-04-15 15:47:21 +0530

    done without animations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plan from './components/Plans/Plan';
 import Program from './components/Program/Program';
+import Regions from './components/Regions/Region'
+import Testimonial from './components/Testimonials/Testimonial'
+import Join from "./components/Join/Join"
+import Footer from './components/Footer/Footer'
 function App() {
   return (
     <div className="App">
       {/* <h1>dhruv</h1> */}
           <Hero/>
           <Program/>
+          <Regions/>
+          <Plan/>
+          <Testimonial/>
+          <Join/>
+          <Footer/>
     </div>
   );
 }
